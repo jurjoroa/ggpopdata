75dfb88d3d02c88a4036c1ec8935dcade6c68586
Jorge Roa <64996320+jurjoroa@users.noreply.github.com>
2024-12-30 14:15:41 -0600

    Erase and update files

---
+++ R/globals.R
@@ -0,0 +1,1 @@
+utils::globalVariables("df_coordinates_final")
--- R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  data("df_coordinates_final", package=pkgname, envir=parent.env(environment()))
-}
