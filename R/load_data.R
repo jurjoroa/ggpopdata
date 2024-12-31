69621d3376938d0a5b13f4170a8dcb0c8b572054
Jorge Roa <64996320+jurjoroa@users.noreply.github.com>
2024-12-30 18:55:46 -0600

    Update load_data.R

--- R/load_data.R
+++ R/load_data.R
@@ -1,4 +1,4 @@
-#' Load coordinates Dataset from .rda File
+#' Load coordinatesDataset from .rda File
 #'
 #' @description
 #' This function loads a dataset from an \code{.rda} file named
@@ -15,9 +15,16 @@
 #' # Inspect the first few rows
 #' head(df_coordinates)
 #' }
-#'
+#' @return A data.frame containing df_coordinates_final
+#' @importFrom utils data
 #' @export
 load_data <- function() {
-  load("data/df_coordinates_final.rda")
-  return(df_coordinates_final)
+  # Create a temporary environment
+  tmp_env <- new.env(parent = emptyenv())
+  
+  # Load df_coordinates_final into the temporary environment
+  data("df_coordinates_final", package = "ggpopdata", envir = tmp_env)
+  
+  # Return the data from tmp_env
+  tmp_env$df_coordinates_final
 }
